bb8c177201b2cfe68ab6e68414c3d03885f33bf7
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-02-14 17:25:00 -0300

    Testes Jest

--- script.test.js
+++ script.test.js
@@ -11,33 +11,55 @@ const { save,
   filterNames, 
 } = require('./script');
 
-describe('Testa a funçao save', () => {
+describe('1 - Testa a funçao save', () => {
   it('Testa se save é uma funçao', () => {
     expect.assertions(1);
     expect(typeof save).toBe('function'); 
   });
 });
 
-describe('Testa a funçao getSaved', () => {
+describe('2 - Testa a funçao getSaved', () => {
   it('Testa se getSaved é uma funçao', () => {
     expect.assertions(1);
     expect(typeof getSaved).toBe('function');
   });
 });
 
-describe('Testa funçao pokeNames', () => {
+describe('3 - Testa funçao pokeNames', () => {
   it('Testa seu retorno', async () => {
     expect.assertions(1);
     const pokeNames = jest.fn(() => true);
     await pokeNames();
     expect(pokeNames).toHaveReturned();
   })
-})
-
-describe('Testa função loadList', () => { 
-  it ('', () => {
-    expect.assertions(1);
-    loadList();
-    expect(fetch).toHaveBeenCalled();
+  it('Testa se typeof pokeNames é objeto', async () => {
+    await expect(typeof pokeNames()).toBe('object');
   })
 })
+
+describe('4 - Testa função loadList', () => { 
+  it ('Testa se loadList é funçao', () => {
+    expect(typeof loadList).toBe('function');
+  });
+});
+
+describe('5 - Testa removeSelected', () => {
+  it('Testa se removeSelected é funçao', () => {
+    expect(typeof removeSelected).toBe('function');
+  });
+});
+
+describe('6 - Testa cardsFunction', () => {
+  it('Testa se cardsFunction é funçao', () => {
+    expect(typeof cardsFunctions).toBe('function');
+  });
+});
+
+describe('7 - Testa filterNames', () => {
+  it('Testa se filterNames é funçao', () => {
+    expect(typeof filterNames).toBe('function');
+  });
+  it('Testa se filterNames retorna objeto', async () => {
+    await expect(typeof filterNames()).toBe('object');
+  })
+});
